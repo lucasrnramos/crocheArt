1506443373560edf1fcaa0e1346cc8325cd69f93
Lucas Ramos <84045235+lucasrnramos@users.noreply.github.com>
2022-05-04 12:51:05 -0300

    Finalização de menu Hamburguer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const btnMobile = document.querySelector('#btn-mobile');
 
-function toggleMenu(){
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault();
     const navBar = document.querySelector('#nav-bar');
     navBar.classList.toggle('active');
+    const active = navBar.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
 }
-
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
