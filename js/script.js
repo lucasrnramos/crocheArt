45506340d33cfe76368a7da1ca0bd61ab4b6895c
Lucas Rodrigues <lucas.ramos18@outlook.com>
2022-10-19 14:27:36 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Menu Hamburguer
 const btnMobile = document.querySelector('#btn-mobile');
 
-function toggleMenu(event){
+function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
     const navBar = document.querySelector('#nav-bar');
     navBar.classList.toggle('active');
@@ -17,6 +17,3 @@ btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
 //Acessando elementos
-
-
-
